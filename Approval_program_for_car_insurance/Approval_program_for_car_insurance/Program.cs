0707367f288097441c8c9c94910c0e6aaccdb78d
kexere <126893696+kexere@users.noreply.github.com>
2024-05-06 11:09:07 +0100

    Added Boolean Logic Assignment

---
+++ Approval_program_for_car_insurance/Approval_program_for_car_insurance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval_program_for_car_insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+
+            Console.WriteLine("\nWhat is your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); //converts string to integer
+
+            Console.WriteLine("\nHave you ever had a DUI? Please answer with yes on no.");
+            string dui = Console.ReadLine().ToLower(); //converts answer to lower letters
+            bool duiAnswer = dui == "yes"; //checks if person have a DUI
+
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine()); //converts string to integer
+
+            bool qualified = (age > 15) && !duiAnswer && (speedingTickets <= 3); //checks if person is qualified
+
+            Console.WriteLine("\nQualified?");
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+
+        }
+    }
+}
