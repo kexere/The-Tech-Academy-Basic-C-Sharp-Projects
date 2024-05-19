4babdc5fa1e03b7e661804e88f7759e5a5b953a2
kexere <126893696+kexere@users.noreply.github.com>
2024-05-19 12:07:16 +0100

    Added Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathMethods
+    {
+        //create three methods that take one integer parameter and return integer
+        //method 1
+        public static int Attack(int strength)
+        {
+            Random rnd = new Random();
+            int enemy_def = rnd.Next(1, 50); //creates random number between 1 and 50
+            int punch_str = rnd.Next(10, 100);
+            int kick_str = rnd.Next(10, 100);
+            int combo_bonus = rnd.Next(20, 60);
+            int crit_chance = rnd.Next(1, 2);
+            int damage = (strength + (punch_str + kick_str) - enemy_def) * crit_chance;
+            return damage;
+        }
+        //method 2
+        public static int Defense(int defense)
+        {
+            Random rnd = new Random();
+            int enemy_str = rnd.Next(1, 50); //creates random number between 1 and 50
+            int punch_str = rnd.Next(10, 100);
+            int kick_str = rnd.Next(10, 100);
+            int combo_bonus = rnd.Next(20, 60);
+            int crit_chance = rnd.Next(1, 2);
+            int enemy_damage = (enemy_str + (punch_str + kick_str) - defense) * crit_chance;
+            return enemy_damage;
+        }
+
+        //method 3 
+        public static int Health(int medicine)
+        {
+            Random rnd = new Random();
+            int getBetter = medicine - rnd.Next(1, medicine);
+            return getBetter;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You are participating in kickbox fight.");
+            Console.WriteLine("Input your defense stat:");
+            int defense = Convert.ToInt32(Console.ReadLine());
+            int enemy_damage = MathMethods.Defense(defense);
+            Console.WriteLine("You have taken " + enemy_damage + " HP of damage.");
+            Console.WriteLine("You find a medicine pouch in your pocket. It heals some of your damage.");
+            Console.WriteLine("Input your medicine pouch stat:");
+            int medicine = Convert.ToInt32(Console.ReadLine());
+            int getBetter = MathMethods.Health(medicine);
+            Console.WriteLine("You have been healed by " + getBetter + " HP.");
+            Console.WriteLine("Now it is your turn to attack.");
+            Console.WriteLine("Input your strength stat:");
+            int strength = Convert.ToInt32(Console.ReadLine());
+            int damage = MathMethods.Attack(strength);
+            Console.WriteLine("Your attack dealt " + damage + " HP worth of damage.");
+            Console.ReadLine();
+        }
+    }
+}
