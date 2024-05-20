96a7b97d57953f46b8afbc9273cadd86068131a1
kexere <126893696+kexere@users.noreply.github.com>
2024-05-20 16:56:54 +0100

    Added Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class MathOperation
+    {
+        //Create void method that takes two integers as paremeters
+      public void PerformOperation(int number1, int number2)
+      {
+            // Perform a math operation on the first integer
+            int result = number1 * 2;
+            // Display the second integer to the screen
+            Console.WriteLine("The second integer is: " + number2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate MathOperation class
+            MathOperation mathOp = new MathOperation();
+
+            //Call the PerformOperation method with two integers
+            mathOp.PerformOperation(10, 20);
+
+            //Call the PerformOperation method with named parameters
+            mathOp.PerformOperation(number1: 3, number2: 20);
+            Console.ReadLine();
+        }
+    }
+}
