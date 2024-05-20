f48a4780dfb8d19240f6d852034125fe82f80dd2
kexere <126893696+kexere@users.noreply.github.com>
2024-05-20 12:14:57 +0100

    Added Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Math class
+            myMath math1 = new myMath();
+            //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(10));
+            //Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(10.5m));
+            //Call a third method with a string parameter
+            Console.WriteLine(math1.MathOp("10"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/myMath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class myMath
+    {
+        //First method that takes an integer from user input
+        public int MathOp(int number)
+        {
+            return number + 10;
+        }
+
+        //Second method that takes a decimal from user input
+        public int MathOp(decimal number)
+        {
+            //Convert the decimal to an int to perform math operation
+            int number2 = Convert.ToInt32(number);
+            return number2 * 30;
+        }
+
+        //Third method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an integer to perform math operation
+            int number2 = Convert.ToInt32(number);
+            return number2 / 10;
+        }
+    }
+}
