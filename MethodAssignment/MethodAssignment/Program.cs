ac98434a8ea266dc60865e29baf98b8be15ee730
kexere <126893696+kexere@users.noreply.github.com>
2024-05-20 12:42:38 +0100

    Added Method Assignment

---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate myMath class
+            myMath Math2 = new myMath();
+
+            //Ask user to provide first parameter
+            Console.WriteLine("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Ask user to provide a second parameter or press enter
+            Console.WriteLine("Enter a second integer, or just press enter: ");
+            try
+            {
+                //Call method with 2 parameters if both parameters are provided
+                int number2 = Convert.ToInt32(Console.ReadLine());
+                int results = Math2.MathOp1(number, number2);
+                Console.WriteLine(number + " + " + number2 + " = " + results);
+            }
+            catch
+            {
+                //Call method with one parameter if only one is provided
+                int results = Math2.MathOp1(number);
+                Console.WriteLine(number + " + Default 1 = " + results);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/myMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class myMath
+    {
+        //Create a method that takes two integers as parameters
+        //Make one paremeter optional by providing a default value
+        public int MathOp1(int number, int number2 = 1)
+        {
+            //Perform addition on integers and return result
+            int result = number + number2;
+            return result;
+        }
+    }
+}
