e3e4c1c98d8846bafe2d21df450c8af9195fff4f
kexere <126893696+kexere@users.noreply.github.com>
2024-05-21 13:15:29 +0100

    Added Methods and Objects Assignment

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main() method, instantiate and initialize an Employee object with
+            //the first name "Sample" and the last name "Student"
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            //Call the superclass method SayName() on the Employee object
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
