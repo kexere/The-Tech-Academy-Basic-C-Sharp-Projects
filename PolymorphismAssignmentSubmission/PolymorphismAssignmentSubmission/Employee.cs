a3a301f411e3e3327e7cf2ccc7c5e89a7a74a9b2
kexere <126893696+kexere@users.noreply.github.com>
2024-05-22 11:20:43 +0100

    Added Polymorphism Assignment

---
+++ PolymorphismAssignmentSubmission/PolymorphismAssignmentSubmission/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignmentSubmission
+{
+    //Inherits from Person class and IQuittable interface
+    public class Employee : Person, IQuittable
+    {
+        //Implement the SayName() method inside of the Employee class.
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+        //Implement the Quit() method from IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("Press 'enter' to quit!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PolymorphismAssignmentSubmission/PolymorphismAssignmentSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an Employee object with firstName “Sample” and lastName “Student”
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+            //Call the SayName() method on the object
+            employee.SayName();
+            //Use polymorphism to create an object of type IQuittable
+            IQuittable quittable = employee;
+            // Call the Quit() method on the IQuittable object
+            quittable.Quit();
+        }
+    }
+}
