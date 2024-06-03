886bcd67e51fc3164f1fe3c6f19c67cfad15f768
kexere <126893696+kexere@users.noreply.github.com>
2024-06-03 13:42:33 +0100

    Added Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Chain
+    {
+        string Name { get; set; }
+        float Num { get; set; }
+        static Random random = new Random();
+
+        //Constructor if one parameter, a float, is provided
+        public Chain(float num1) : this(num1, "Mistery Person")
+        {
+            Console.Write(Name + ", your favorite number is " + num1);
+        }
+        //Constructor if one parameter, a string, is provided
+        public Chain(string name1) : this(random.Next(0, 100), name1)
+        {
+            Console.Write(name1 + ", your favorite random number is " + Num);
+        }
+        //Constructor if both parameters, a string and a float, are provided
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a const variable
+            const string const1 = "Hello ";
+            Console.WriteLine("Enter your name or favorite number: ");
+            //Create a variable using var keyword
+            var answer = Console.ReadLine();
+
+            if (float.TryParse(answer, out float x))
+            {
+                Console.Write(const1);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write(const1);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
