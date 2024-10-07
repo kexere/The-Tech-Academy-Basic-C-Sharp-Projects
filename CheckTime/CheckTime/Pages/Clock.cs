a748a8fe63ed51e48bfb77853ccf40e069d72258
kexere <126893696+kexere@users.noreply.github.com>
2024-10-07 13:04:38 +0100

    Added Challenge Assignment

---
+++ CheckTime/CheckTime/Pages/Clock.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckTime.Pages
+{
+    public class Clock
+    {
+        public static string GetTime()
+        {
+            DateTime localDate = DateTime.Now;
+            var clockMessage = "Your local time is ";
+            return clockMessage + localDate.ToString();
+        }
+    }
+}
