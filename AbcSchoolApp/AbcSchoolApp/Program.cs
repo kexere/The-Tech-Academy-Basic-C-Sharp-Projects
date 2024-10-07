27c310cd7f16737af06c373715a3acd422f407c5
kexere <126893696+kexere@users.noreply.github.com>
2024-10-07 14:35:18 +0100

    Added Final Assignment

---
+++ AbcSchoolApp/AbcSchoolApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using AbcSchoolApp;
+using System.Data.Entity;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AbcSchoolApp.Models
+{
+    class Program
+    {
+         public static void Main(string[] args)
+         {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Elsa"};
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!\n");
+
+                foreach (var student in ctx.Students.ToList())
+                {
+                    Console.WriteLine($"Student ID: {student.StudentId}, Name: {student.StudentName}");
+                }
+
+            }
+         }
+    }
+}
